20e934e365cc788f0b2ad61f35c9ca37afab8253
Vimal MR <78142299+Vimalmr@users.noreply.github.com>
2021-10-01 15:01:34 +0500

    Little modification and optimisation
    This commit is due to optimisation to the assignment they given

--- Password_checker.c
+++ Password_checker.c
@@ -5,7 +5,6 @@
 int check_password(char *password){
     int a=0,b=0,c=0,d=0;
     if(strlen(password) >= 8 ){
-        printf("Strong\n");
         for(int i=0; i<strlen(password); i++){
             if(isupper(password[i])){
                 b++; //b=upper
@@ -21,56 +20,51 @@ int check_password(char *password){
             }
         }
         if(a<3 && b==0 && c==0){
-            printf("1\n");
             return 1;
         }
         else if(b==0 && c==0 && d<3){
-            printf("2\n");
             return 2;
         }
         else if(c==0 && d<3 && a<3){
-            printf("3\n");
             return 3;
         }
         else if(d<3 && a<3 && b==0){
-            printf("4\n");
             return 4;
         }
         else if((a<3 && b==0) || (c==0 && d<3)){
-            printf("5\n");
             return 5;
         }
         else if((b==0 && c==0) || (d<3 && a<3)){
-            printf("6\n");
             return 6;
         }
         else if(a<3 || b==0){
-            printf("7\n");
             return 7;
         }
         else if(c==0 || d<3){
-            printf("8\n");
             return 8;
         }
         else if(a>3 && b!=0 && c!=0 && d>3){
-            printf("9\n");
             return 9;
         }
         else{
-            printf("0\n");
             return 0;
         }
     }
     else{
-        printf("0\n");
         return 0;
     }
 }
 
 int main(){
-    char paswrd[20];
-    printf("Enter Password: \n");
+    char paswrd[25];
+    printf("1. Password must be minimum of 8 characters. \n");
+    printf("2. Password must have one Uppercase Character. \n");
+    printf("3. Password must have three Lowercase Characters. \n");
+    printf("4. Password must have one symbol. \n");
+    printf("5. Password must have three Numeric Characters that are not sequential conditions \n");
+    printf("Enter Password: ");
     scanf( "%s", paswrd);
+    printf("\n");
     int rank = check_password(paswrd);
     if(rank==0){
         printf("Weakest Password\n");
@@ -82,16 +76,22 @@ int main(){
         printf("average Password\n");
     }
     else if(rank==5 || rank==6){
-        printf("medium Passwordn\n");
+        printf("medium Password\n");
     }
     else if(rank==7 || rank==8){
-        printf("strong Passwordn\n");
+        printf("strong Password\n");
     }
     else if(rank==9){
         printf("strongest Password\n");
     }
     return 0;
 }
+//
+//
+//
+//
+//
+//
 //
 //
 //
